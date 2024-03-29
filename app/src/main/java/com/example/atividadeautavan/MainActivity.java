4b6b0763356cea19f7bd0782724f64515f9ba957
vitor-laudares <vitor.laudares.eng@gmail.com>
2024-03-29 15:23:50 -0300

    Sexto Commit - Menu para tipo de mapa + controle de zoom

--- app/src/main/java/com/example/atividadeautavan/MainActivity.java
+++ app/src/main/java/com/example/atividadeautavan/MainActivity.java
@@ -6,6 +6,8 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationRequest;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -101,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     currentLocation = location;
 
                     textViewLabel3.setText("Latitude: " + String.valueOf(currentLocation.getLatitude()));
-                    textViewLabel4.setText("Latitude: " + String.valueOf(currentLocation.getLongitude()));
+                    textViewLabel4.setText("Longitude: " + String.valueOf(currentLocation.getLongitude()));
 
                     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
                     mapFragment.getMapAsync(MainActivity.this);
@@ -121,6 +123,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MarkerOptions options = new MarkerOptions().position(sydney).title("Minha Localização");
         options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
         myMap.addMarker(options);
+        myMap.getUiSettings().setZoomControlsEnabled(true);
 
     }
 
@@ -134,5 +137,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id==R.id.mapNone){
+            myMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        }
+        if(id==R.id.mapNormal){
+            myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+        if(id==R.id.mapSatellite){
+            myMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        if(id==R.id.mapHybrid){
+            myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
+        if(id==R.id.mapTerrain){
+            myMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
